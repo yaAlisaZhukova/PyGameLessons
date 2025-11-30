b1624efe82eab8e8f07eedd52c4968c2b32d69be
elena <lenchik.lena.len@gmail.com>
2025-11-30 10:58:55 -0600

    names

--- 06_AlisaCatKey/main.py
+++ 06_AlisaCatKey/main.py
@@ -11,52 +11,52 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Cat')
 
 brez = pygame.transform.scale(pygame.image.load('img/briz.png'),(70, 70))
-brezRect = brez.get_rect(topleft=(50, 50))
+brez_rect = brez.get_rect(topleft=(50, 50))
 
-goldenKey = pygame.transform.scale(pygame.image.load('img/key.png'),(60, 60))
+golden_key = pygame.transform.scale(pygame.image.load('img/key.png'),(60, 60))
 key_rect_list = []
 
-def addKeys(level_key_number):
+def add_keys(level_key_number):
     for i in range(level_key_number):
-        keyRect = goldenKey.get_rect(topleft=(random.randint(0, 800), random.randint(0, 600)))
+        keyRect = golden_key.get_rect(topleft=(random.randint(0, 800), random.randint(0, 600)))
         key_rect_list.append(keyRect)
 
 level_key_number = 2
-addKeys(level_key_number)
+add_keys(level_key_number)
 
 WHITE = (255,255,255)
 clock = pygame.time.Clock()
 
-def keyPressed():
+def key_pressed():
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-        brezRect.x -= 1
+        brez_rect.x -= 1
     if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-        brezRect.x += 1
+        brez_rect.x += 1
     if keys[pygame.K_UP] or keys[pygame.K_w]:
-        brezRect.y -= 1
+        brez_rect.y -= 1
     if keys[pygame.K_DOWN] or keys[pygame.K_s]:
-        brezRect.y += 1
+        brez_rect.y += 1
 
 run = True
 while run:
     clock.tick(90)
-    keyPressed()
+    key_pressed()
     screen.fill(WHITE)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    screen.blit(brez, brezRect)
+    screen.blit(brez, brez_rect)
 
     if len(key_rect_list) == 0:
         level_key_number += 5
-        addKeys(level_key_number)
+        add_keys(level_key_number)
 
     for i in range(len(key_rect_list)):
-        screen.blit(goldenKey, key_rect_list[i])
-        if brezRect.colliderect(key_rect_list[i]):
+        screen.blit(golden_key, key_rect_list[i])
+        if brez_rect.colliderect(key_rect_list[i]):
             key_rect_list.pop(i)
             break
 
